fdb5c666a411c0a42f0ec1737c9e56f7857dedc7
14646-sys <14646@holbertonstudents.com>
2026-06-23 17:40:50 +0200

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char lettre
+
+	lettre = 'z';
+	while (lettre >= 'a')
+	{
+	putchar(lettre);
+	lettre--;
+	}
+	putchar('\n');
+return (0);
+}
