6be66d12ad2d874fd7fda7cd2d361dd8e1b405dc
14646-sys <14646@holbertonstudents.com>
2026-06-24 17:51:00 +0200

    Update main.h et file 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an interger
+*
+* Return: The absolute value of n
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
