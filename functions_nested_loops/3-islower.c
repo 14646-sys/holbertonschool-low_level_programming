e73fbd3a17d64ce6c2da1e2837d608c39e5a0a9a
14646-sys <14646@holbertonstudents.com>
2026-06-24 15:14:17 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+* islower(int c) - writes a function that checks for lowercase character
+*
+* Return: 1 if lowercase, 0 otherwise
+*/
+
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
+
