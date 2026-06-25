eb1d4ace6ddc3cbd8af0280425376853cb5e63de
14646-sys <14646@holbertonstudents.com>
2026-06-25 09:41:22 +0200

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_last_digit - prints the last digit of a number
+* @n: the number to use
+*
+* Return: the value of the last digit
+*/
+int print_last_digit(int n)
+{
+	int last;
+	
+	last = n % 10;
+	if (last < 0)
+	last = -last;
+	putchar(last );
+	return (n
+	}
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
