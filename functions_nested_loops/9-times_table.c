8cee14e3db5e3b63bd781020f2ab5bdcd7e811bb
14646-sys <14646@holbertonstudents.com>
2026-06-25 17:38:29 +0200

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -30,7 +30,8 @@ void times_table(void)
                 _putchar('0' + (result / 10));
                 _putchar('0' + (result % 10));
             }
-            _putchar('\n');
 	    }
+        _putchar('\n');
     }
+
 }
