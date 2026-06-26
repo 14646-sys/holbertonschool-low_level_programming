ebf8669263fcc2cb1cb65d00ddd4abfdee3261e5
14646-sys <14646@holbertonstudents.com>
2026-06-26 13:44:44 +0200

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,32 +5,32 @@
 */
 void times_table(void)
 {
-    int table, table_count, result;
+	int table, table_count, result;
 
-    for (table = 0; table <= 9; table++)
-    {
-        for (table_count = 0; table_count <= 9; table_count++)
-        {
-            result = table * table_count;
-            if (table_count == 0)
-            {
-                _putchar('0' + result);
-            }
-            else if (result <= 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar('0' + result);
-            }
-            else if (result > 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar('0' + (result / 10));
-                _putchar('0' + (result % 10));
-            }
-	    }
-    _putchar('\n');
-    }
+	for (table = 0; table <= 9; table++)
+	{
+		for (table_count = 0; table_count <= 9; table_count++)
+		{
+			result = table * table_count;
+			if (table_count == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
